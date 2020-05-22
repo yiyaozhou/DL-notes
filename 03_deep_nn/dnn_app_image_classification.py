8f69331b5580722b9a1f292be1c944175aab71af
yiyaozhou <yiyaozhou@brandeis.edu>
2020-05-22 17:58:16 -0400

    build a deep neural network for cat or non-cat classification

--- 03_deep_nn/dnn_app_image_classification.py
+++ 03_deep_nn/dnn_app_image_classification.py
@@ -20,9 +20,9 @@ np.random.seed(1)
 
 train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
 # Example of a picture
-index = 10
-plt.imshow(train_x_orig[index])
-print ("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
+# index = 10
+# plt.imshow(train_x_orig[index])
+# print ("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
 
 # Explore your dataset 
 m_train = train_x_orig.shape[0]
@@ -49,67 +49,17 @@ print ("train_x's shape: " + str(train_x.shape))
 print ("test_x's shape: " + str(test_x.shape))
 
 
-### CONSTANTS DEFINING THE MODEL ####
-# n_x = 12288     # num_px * num_px * 3
-# n_h = 7
-# n_y = 1
-# layers_dims = (n_x, n_h, n_y)
+## CONSTANTS DEFINING THE MODEL ####
+n_x = 12288     # num_px * num_px * 3
+n_h = 7
+n_y = 1
+layers_dims = (n_x, n_h, n_y)
 
-# two_layer_model
-# parameters = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost=True)
-# predictions_train = predict(train_x, train_y, parameters)
-# predictions_test = predict(test_x, test_y, parameters)
-def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):#lr was 0.009
-    """
-    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
-    
-    Arguments:
-    X -- data, numpy array of shape (num_px * num_px * 3, number of examples)
-    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
-    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
-    learning_rate -- learning rate of the gradient descent update rule
-    num_iterations -- number of iterations of the optimization loop
-    print_cost -- if True, it prints the cost every 100 steps
-    
-    Returns:
-    parameters -- parameters learnt by the model. They can then be used to predict.
-    """
+two_layer_model
+parameters = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost=True)
+predictions_train = predict(train_x, train_y, parameters)
+predictions_test = predict(test_x, test_y, parameters)
 
-    np.random.seed(1)
-    costs = []                         # keep track of cost
-    
-    # Parameters initialization. (≈ 1 line of code)
-    parameters = initialize_parameters_deep(layers_dims)
-    
-    # Loop (gradient descent)
-    for i in range(0, num_iterations):
-
-        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
-        AL, caches = L_model_forward(X, parameters)
-        
-        # Compute cost.
-        cost = compute_cost(AL, Y)
-    
-        # Backward propagation.
-        grads = L_model_backward(AL, Y, caches)
- 
-        # Update parameters.
-        parameters = update_parameters(parameters, grads, learning_rate)
-                
-        # Print the cost every 100 training example
-        if print_cost and i % 100 == 0:
-            print ("Cost after iteration %i: %f" %(i, cost))
-        if print_cost and i % 100 == 0:
-            costs.append(cost)
-            
-    # plot the cost
-    plt.plot(np.squeeze(costs))
-    plt.ylabel('cost')
-    plt.xlabel('iterations (per hundreds)')
-    plt.title("Learning rate =" + str(learning_rate))
-    plt.show()
-    
-    return parameters
 
 ### CONSTANTS ###
 #layers_dims = [12288, 20, 7, 5, 1] #  4-layer model
